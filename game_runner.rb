2e0c3c09a3161ddd9538c780ebfb1f16dfef79f2
nickhillwd <nickhillwd@gmail.com>
2015-11-25 10:24:25 +0000

    board function printing to console

--- game_runner.rb
+++ game_runner.rb
@@ -7,12 +7,14 @@ game = Game.new()
 #place piece
 game.place_piece(0,0)
 
+#display board
+puts game.display_board
+
+#test display row
+puts "test row #{game.row_to_string([:o,nil,nil])}"
+
+
 #binding.pry;''
-
-
-
-
-
 
 
 
