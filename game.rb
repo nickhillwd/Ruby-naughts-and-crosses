3497f102f037c694f7b9597c3ddeb86d1d929f16
nickhillwd <nickhillwd@gmail.com>
2015-11-25 13:36:21 +0000

    addded functionality to auto check if the board if full and reset

--- game.rb
+++ game.rb
@@ -7,7 +7,7 @@ class Game
   end
 
   def place_piece(row, column)
-    if row > 2 || column > 2
+    if out_of_range?(row, column)
       puts "No silly, play in the board"
       puts self.display_board
       return
@@ -20,6 +20,12 @@ class Game
     @board[ row ][ column ] = @pieces[ @turn % 2 ]
     @turn = @turn + 1
     puts self.display_board
+
+    if board_full?
+      puts "Board full, play again."
+      restart
+    end
+
   end
 
   def display_board
@@ -36,7 +42,16 @@ class Game
     puts self.display_board
   end
 
+
     private
+
+    def full_board?
+      @board.flatten.all?
+    end
+
+    def out_of_range?(row, column)
+      row > 2 || column > 2
+    end
 
     def row_to_string(row)
       row_symbols = row.map do |piece|
